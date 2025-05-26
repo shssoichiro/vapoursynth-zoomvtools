f910a9cece998b68697df4acedac035569b288d5
Josh Holmer <jholmer.in@gmail.com>
2025-05-26 13:05:28 -0400

    Set up first modules

--- benches/bench.rs
+++ benches/bench.rs
@@ -15,5 +15,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+criterion_group!(mv_analyse, criterion_benchmark);
+criterion_group!(mv_compensate, criterion_benchmark);
+criterion_group!(mv_recalculate, criterion_benchmark);
+criterion_group!(mv_super, criterion_benchmark);
+criterion_main!(mv_analyse, mv_compensate, mv_recalculate, mv_super);
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use vapoursynth::{export_vapoursynth_plugin, plugins::Metadata};
 
+mod mv_analyse;
+mod mv_compensate;
+mod mv_recalculate;
+mod mv_super;
+
 pub const PLUGIN_IDENTIFIER: &str = "com.soichiro.zoomvtools";
 pub const PLUGIN_NAME: &str = "zoomvtools";
 
